6cfddf421d336495f2f73c1c45043697dcfba2e2
toitsuno <toyafi0628@gmail.com>
2019-06-11 21:36:43 +0900

    初めてのGitHub！

---
+++ index.php
@@ -0,0 +1,61 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ポートフォリオ</title>
+		<link rel="stylesheet"  type="text/css" href="main.css">
+        
+	</head>
+	<body>
+		<header>
+			
+			<ul>
+				<li><a href="" id="header-content">ABOUT</a></li>
+				<li><a href="" class="header-content">worK</a></li>
+				<li><a href="" class="header-content">SKILL</a></li>
+				<li><a href="" class="header-content">CONTACT</a></li>
+				<li><a href="" class="header-top">私について</a></li>
+			</ul>
+		</header>
+		<main>
+			
+			<div class="top-content">
+				<div id="particle"></div>
+				<h2 id="top-title">TOI TSUNO</h2>
+				<span><a href="" id="about-btn">about me</a><span>
+			</div>
+			<div class="about-content">
+				<h2>ABOUT</h2>
+				<p>津野　透生（つの　とうい)</p>
+			</div>
+			<div class="skill-content">
+				<h2>SKILL</h2>
+				<p>私が使用できる言語はこちらです</p>
+			</div>
+			</div>
+			
+		</main>
+		<footer>
+			
+			<ul>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+				<li><a href=""></a></li>
+			</ul>
+		</footer>
+		
+
+　　　
+
+
+
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="main.js"></script>
+    <script src="jquery.particleground.min.js"></script>
+
+	</body>
+</html>
